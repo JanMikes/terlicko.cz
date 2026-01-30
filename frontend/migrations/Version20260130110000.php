afe4da4a0b2458cc8df6652eadf1ead6b436a241
Jan Mikeš <j.mikes@me.com>
2026-01-30 11:12:15 +0100

    Renaming

--- frontend/migrations/Version20260130110000.php
+++ frontend/migrations/Version20260130110000.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260130095938 extends AbstractMigration
+final class Version20260130110000 extends AbstractMigration
 {
     public function getDescription(): string
     {
